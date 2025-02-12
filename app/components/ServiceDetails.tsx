bc6b83e4c447332645f0095adb3fe9737884d958
Abdul Qadoos <87199809+abdulqadoos-dev@users.noreply.github.com>
2025-02-12 19:11:01 +0500

    contact form in services page

--- app/components/ServiceDetails.tsx
+++ app/components/ServiceDetails.tsx
@@ -2,54 +2,125 @@
 
 import React from 'react';
 
+import Button from "@/app/components/ui/Button";
+
 interface Section {
-  heading: string | null;
-  content: string | null; 
-  list_items?: string[]; 
+   heading: string | null;
+   content: string | null;
+   list_items?: string[];
 }
 
 interface Article {
-  title: string;
-  slug: string;
-  sections: Section[];
+   title: string;
+   slug: string;
+   sections: Section[];
 }
 
 const ServiceDetails: React.FC<{ article: Article }> = ({ article }) => {
-  if (!article) {
-    return <div>Loading...</div>;
-  }
+   if (!article) {
+      return <div>Loading...</div>;
+   }
 
-  return (
-    <article className="relative min-h-screen bg-secondary bg-no-repeat bg-cover py-36">
-      <div className="absolute inset-0 bg-gradient-radial from-white/15 to-secondary z-10" />
+   return (
+      <article className="relative min-h-screen bg-secondary bg-no-repeat bg-cover py-36">
+         <div className="absolute inset-0 bg-gradient-radial from-white/15 to-secondary z-10" />
 
-      <div className="container mx-auto relative z-20">
-        <h2 className="text-white md:text-7xl text-3xl my-8 mx-10 font-bold">
-          {article.title}
-        </h2>
+         <div className="container mx-auto relative z-20">
+            <h2 className="text-white md:text-7xl text-3xl my-8 mx-10 font-bold">
+               {article.title}
+            </h2>
 
-        <div className="grid md:grid-cols-3 gap-10 items-start">
-          <div className="bg-black text-white md:col-span-2 space-y-10 p-10 rounded-xl">
-            {article.sections.map((section, index) => (
-              <div className="space-y-5" key={index}>
-                {section.heading && <h2 className="text-4xl">{section.heading}</h2>}
-                {section.content && <p>{section.content}</p>}
-                {section.list_items && (
-                  <ul className="list-disc pl-6 space-y-2"> 
-                    {section.list_items.map((item, itemIndex) => (
-                      <li key={itemIndex}>{item}</li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            ))}
-          </div>
+            <div className="grid md:grid-cols-3 gap-10 items-start">
+               <div className="bg-black text-white md:col-span-2 space-y-10 p-10 rounded-xl">
+                  {article.sections.map((section, index) => (
+                     <div className="space-y-5" key={index}>
+                        {section.heading && <h2 className="text-4xl">{section.heading}</h2>}
+                        {section.content && <p>{section.content}</p>}
+                        {section.list_items && (
+                           <ul className="list-disc pl-6 space-y-2">
+                              {section.list_items.map((item, itemIndex) => (
+                                 <li key={itemIndex}>{item}</li>
+                              ))}
+                           </ul>
+                        )}
+                     </div>
+                  ))}
+               </div>
 
-          <div className="bg-black text-white space-y-10 p-10 rounded-xl"></div>
-        </div>
-      </div>
-    </article>
-  );
+               <div className="bg-black text-white space-y-10 p-10 rounded-xl">
+
+                  <p className="text-lg text-white/80 font-bold">
+                     Get in touch
+                  </p>
+
+                  <form className="space-y-6">
+                     <div>
+                        <label htmlFor="name" className="block text-sm font-medium text-white/70">
+                           Name
+                        </label>
+                        <input
+                           type="text"
+                           id="name"
+                           required
+                           className="mt-1 p-3 bg-white/10 backdrop-blur-lg block w-full rounded-lg text-white focus:outline-none "
+                        />
+                     </div>
+
+                     <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-white/70">
+                           Email
+                        </label>
+                        <input
+                           type="email"
+                           id="email"
+                           required
+                           className="mt-1 p-3 bg-white/10 backdrop-blur-lg block w-full rounded-lg text-white focus:outline-none"
+                        />
+                     </div>
+
+                     <div>
+                        <label htmlFor="message" className="block text-sm font-medium text-white/70">
+                           Message
+                        </label>
+                        <textarea
+                           id="message"
+                           rows={4}
+                           required
+                           className="mt-1 p-3 bg-white/10 backdrop-blur-lg block w-full rounded-lg text-white focus:outline-none"
+                        ></textarea>
+                     </div>
+
+                     <Button variant="primary" size="lg" className="lg:w-auto min-w-40"> Send Message</Button>
+                  </form>
+
+                  <div className="container mx-auto px-4 flex flex-col justify-between  gap-10">
+
+                     <div className="flex flex-col gap-2">
+                        <span className="text-5xl text-white/15 font-bold">01</span>
+                        <a href="mailto:constact@softdevai.com" className="text-xl font-bold text-white/80">
+                           info@jerika.ca
+                        </a>
+
+                     </div>
+
+                     <div className="flex flex-col gap-2">
+                        <span className="text-5xl text-white/15 font-bold">02</span>
+                        <a href="" className="text-xl font-bold text-white/80">
+                           (519) 788-9917 , (647) 270-6400
+                        </a>
+                     </div>
+
+                     <div className="flex flex-col gap-2">
+                        <span className="text-5xl text-white/15 font-bold">03</span>
+                        <p className="text-xl font-bold text-white/80 ">1 King Street West, Toronto, ON M5H 1A1</p>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </article>
+   );
 };
 
 export default ServiceDetails;
--- app/components/ServicesGrid.tsx
+++ app/components/ServicesGrid.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+
+import Link from "next/link";
+
 const ServicesGrid = () => {
 
+
+  const services = [
+    { number: "01", title: "Managed IT Services", "slug": "jerika-inc-managed-it-services" },
+    { number: "02", title: "Managed Security Services", "slug": "managed-security-services" },
+    { number: "03", title: "Data Backup + Disaster Recovery", "slug": "data-backup-disaster-recovery" },
+    { number: "04", title: "Web Development and Hosting", "slug": "web-development-hosting" },
+    { number: "05", title: "Co-Managed IT Services", "slug": "co-managed-it-services" },
+    { number: "06", title: "Cloud Hosting Services", "slug": "cloud-hosting-services" },
+    { number: "08", title: "Social Media Marketing and Branding", "slug": "social-media-marketing-branding" },
+ ]
 
 
   return (
@@ -12,7 +25,8 @@ const ServicesGrid = () => {
 
       <div className="container mx-auto px-4 relative z-20">
         {/* Header */}
-
+    
+      
 
         <div className="text-center mb-16 space-y-4">
           <h2 className="text-4xl md:text-5xl font-bold text-white">
@@ -25,33 +39,33 @@ const ServicesGrid = () => {
 
         <div className="grid grid-cols-11 grid-rows-6 gap-4 h-[60vh] p-4 relative">
 
-          <div className=" col-start-1 col-end-3 row-start-1 row-end-5 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-fade-in ">
+          <Link href={`services/jerika-inc-managed-it-services`} className=" col-start-1 col-end-3 row-start-1 row-end-5 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-fade-in ">
             <h2 className="text-xl font-bold mb-2">Managed IT Services</h2>
-          </div>
+          </Link>
 
-          <div className=" col-start-1 col-end-3 row-start-5 row-end-7 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-slide-in-up delay-200 ">
+          <Link href={`services/managed-security-services`} className=" col-start-1 col-end-3 row-start-5 row-end-7 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-slide-in-up delay-200 ">
             <h2 className="text-xl font-bold mb-2">Managed Security Services</h2>
-          </div>
+          </Link>
 
-          <div className=" col-start-3 col-end-6 row-start-1 row-end-3 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-scale-in delay-300 ">
+           <Link href={`services/data-backup-disaster-recovery`} className=" col-start-3 col-end-6 row-start-1 row-end-3 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-scale-in delay-300 ">
             <h2 className="text-xl font-bold mb-2">Data Backup + Disaster Recovery</h2>
-          </div>
+          </Link>
 
-          <div className=" col-start-6 col-end-10 row-start-1 row-end-3 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-slide-in-right delay-400 ">
+           <Link href={`services/co-managed-it-services`} className=" col-start-6 col-end-10 row-start-1 row-end-3 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-slide-in-right delay-400 ">
             <h2 className="text-xl font-bold mb-2">Co-Managed IT Services</h2>
-          </div>
+          </Link>
 
-          <div className=" col-start-3 col-end-10 row-start-3 row-end-7 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-8 animate-fade-in delay-500 ">
+           <Link href={`services/web-development-hosting`} className=" col-start-3 col-end-10 row-start-3 row-end-7 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-8 animate-fade-in delay-500 ">
             <h2 className="text-2xl font-bold mb-2">Web Development and Hosting</h2>
-          </div>
+          </Link>
 
-          <div className=" col-start-10 col-end-12 row-start-1 row-end-3 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-rotate-in delay-600 ">
+           <Link href={`services/cloud-hosting-services`} className=" col-start-10 col-end-12 row-start-1 row-end-3 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-rotate-in delay-600 ">
             <h2 className="text-xl font-bold mb-2">Cloud Hosting Services</h2>
-          </div>
+          </Link>
 
-          <div className=" col-start-10 col-end-12 row-start-3 row-end-7 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-slide-in-left delay-700 ">
+           <Link href={`services/social-media-marketing-branding`} className=" col-start-10 col-end-12 row-start-3 row-end-7 bg-white/5  rounded-3xl flex flex-col items-center justify-center text-white text-center p-4 animate-slide-in-left delay-700 ">
             <h2 className="text-xl font-bold mb-2">Social Media Marketing and Branding</h2>
-          </div>
+          </Link>
         </div>
       </div>
     </section>
